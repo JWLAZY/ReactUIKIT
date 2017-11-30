ef1473da09c3d2a99a6bad5a7ece6ca7fc53dafb
郑建文 <zhengjianwen@outlook.com>
2017-11-30 10:50:32 +0800

    完善list/item

--- src/List/Item.jsx
+++ src/List/Item.jsx
@@ -1,6 +1,8 @@
 /* @flow */
 import React, { PropTypes } from 'react';
 import './styles/list.scss';
+import arrow from '../../static/Images/idg_application_list_arrow.png';
+// my_profile_library_card_arrow
 type Props = {
   name: string,
   image: string
@@ -11,13 +13,20 @@ class Item extends React.Component<void, Props, void> {
 
   };
 
+  delete(e){
+    this.props.delete();
+    e.preventDefault();
+  }
+  
   render () {
+    let link = this.props.manage ? '' : (this.props.link? <div className="link"><img src={arrow} alt=""/></div> : '');
     return (
-      <div className='list_item_container'>
+      <div className='list_item_container' onClick={this.props.manage ? this.delete.bind(this) : this.props.onClick}>
+        {this.props.manage?(<div className='delete-btn'>一</div>):''} 
         <div className="list_item_info">
           {this.props.children}
         </div>
-        {this.props.link? <div className="link">></div> : ''}
+        {link}
       </div>
     );
   }
