e2b82dd45a40210b502534de719ba9949fbb336e
郑建文 <zhengjianwen@outlook.com>
2018-01-03 16:29:29 +0800

    更改npm源

--- src/List/Item.jsx
+++ src/List/Item.jsx
@@ -2,6 +2,7 @@ import React, { PropTypes,PureComponent } from 'react';
 import '../../styles/list.scss';
 
 class Item extends PureComponent {
+
   delete(e){
     this.props.delete();
     e.preventDefault();
@@ -9,7 +10,7 @@ class Item extends PureComponent {
   
   render() {
     return (
-      <div className='list_item_container' onClick={this.props.manage ? this.delete.bind(this) : this.props.onClick}>
+        <div className='list_item_container' onClick={this.props.manage ? this.delete.bind(this) : this.props.onClick}>
           {this.props.manage? (<div className='delete-btn'>一</div>) : ''} 
           <div className="list_item_info">
             {this.props.children}
